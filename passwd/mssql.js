de42b721692573b2050bb2f8214f2c57f3333d1d
wgcairui <wgcairui@icloud.com>
2019-03-17 13:59:28 +0800

    add mssql async/wait

---
+++ passwd/mssql.js
@@ -0,0 +1,11 @@
+var contstr = {
+    user:'sa',
+    password:'taoyuanxxl',
+    server:'115.29.238.253',
+    database:'xxl_c',
+    port:1433,
+    options:{
+        tdsVersion:'7_1'
+    }
+};
+module.exports = contstr;
